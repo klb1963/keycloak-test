f3aafafaca9fa094a4a7b31b4120cc16f963e08f
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-09-17 18:37:34 +0200

    Initial commit

---
+++ src/main/java/com/sbexample/keycloaktest/KeycloakTestApplication.java
@@ -0,0 +1,13 @@
+package com.sbexample.keycloaktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakTestApplication.class, args);
+	}
+
+}
